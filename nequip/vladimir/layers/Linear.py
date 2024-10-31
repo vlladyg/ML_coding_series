64f6899d62c264ad93bd9fc39ba94d156b76f59a
dreamnewworld <vladygin@caltech.edu>
2024-10-30 22:38:55 -0700

    Wrote tests and started decomposing the model

---
+++ nequip/vladimir/layers/Linear.py
@@ -0,0 +1,30 @@
+from .gmm import GraphModuleMixin
+from e3nn.o3 import Linear
+
+class AtomwiseLinear(GraphModuleMixin, torch.nn.Module):
+    def __init__(
+        self,
+        field: str = AtomicDataDict.NODE_FEATURES_KEY,
+        out_field: Optional[str] = None,
+        irreps_in=None,
+        irreps_out=None,
+    ):
+        super().__init__()
+        self.field = field
+        out_field = out_field if out_field is not None else field
+        self.out_field = out_field
+        if irreps_out is None:
+            irreps_out = irreps_in[field]
+
+        self._init_irreps(
+            irreps_in=irreps_in,
+            required_irreps_in=[field],
+            irreps_out={out_field: irreps_out},
+        )
+        self.linear = Linear(
+            irreps_in=self.irreps_in[field], irreps_out=self.irreps_out[out_field]
+        )
+
+    def forward(self, data: AtomicDataDict.Type) -> AtomicDataDict.Type:
+        data[self.out_field] = self.linear(data[self.field])
+        return data
